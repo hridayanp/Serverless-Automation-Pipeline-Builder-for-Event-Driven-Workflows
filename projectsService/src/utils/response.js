27942267dd70477f889d5dac4b2244804506db5d
hridayanp <hridayanphukan123@gmail.com>
2026-02-26 12:12:27 +0530

    extra

--- projectsService/src/utils/response.js
+++ projectsService/src/utils/response.js
@@ -1,10 +1,38 @@
+// src/utils/response.js
+
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*', // change to frontend URL in prod
   'Access-Control-Allow-Headers': 'Content-Type,Authorization',
-  'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+  'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,DELETE,PUT',
 };
 
-export const success = (data, message = 'Success') => {
+// ------------------------------------------------------------------
+// Standardized API response class (USED EVERYWHERE)
+// ------------------------------------------------------------------
+export class CustomResponse {
+  constructor() {
+    this.success = true;
+    this.message = '';
+    this.data = null;
+  }
+
+  send(statusCode = 200) {
+    return {
+      statusCode,
+      headers: corsHeaders,
+      body: JSON.stringify({
+        success: this.success,
+        message: this.message,
+        data: this.data,
+      }),
+    };
+  }
+}
+
+// ------------------------------------------------------------------
+// Simple helper functions (optional usage)
+// ------------------------------------------------------------------
+export const success = (data = null, message = 'Success') => {
   return {
     statusCode: 200,
     headers: corsHeaders,
@@ -23,6 +51,7 @@ export const error = (message = 'Something went wrong', statusCode = 400) => {
     body: JSON.stringify({
       success: false,
       message,
+      data: null,
     }),
   };
 };
